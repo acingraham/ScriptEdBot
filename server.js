6bf3cc72e6b79b14dcb700d0f36b374adc005ff2
Andrew Ingraham <acingraham@gmail.com>
2014-04-30 03:28:09 +0000

    Returning files with appropriate content types

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var http = require('http'),
-	fs = require('fs');
-
-var root = __dirname;
+	fs = require('fs'),
+	mime = require('mime');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello world\n');
+	var filepath = __dirname + req.url;
+
+	console.log(filepath);
+
+	fs.readFile(filepath, function(err, data) {
+		if(err) {
+			res.writeHead(400, {'Content-Type': 'text/plain'});
+			res.end('File not found');
+		} else {
+			res.writeHead(200, {'Content-Type': mime.lookup(filepath)});
+			res.end(data);
+		}
+	});
+
 }).listen(8000);
+
 console.log('Server running at http://127.0.0.1:8000/');
